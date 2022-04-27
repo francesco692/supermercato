395bd4d90dc2cdf7be16de749b5f34880ea44b0e
Francesco <francesco.serra8280@outlook.it>
2022-04-27 20:02:17 +0200

    added my project

---
+++ src/main/java/com/superm/supermercato/SupermercatoApplication.java
@@ -0,0 +1,13 @@
+package com.superm.supermercato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superm/supermercato/SupermercatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superm.supermercato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
